472bb3c06a9b673ffa2c260c57ee0aa8548619d4
Daniel Rengifo <drensan@hotmail.com>
2018-04-19 15:41:31 -0500

    Tablas en BCNF

--- java-server-master/src/vos/Operador.java
+++ java-server-master/src/vos/Operador.java
@@ -3,24 +3,12 @@ package vos;
 import org.codehaus.jackson.annotate.JsonProperty;
 
 public class Operador extends Usuario {
-
-	@JsonProperty(value="nombre")
-	private String nombre;
 	
 	public Operador(@JsonProperty(value="usuario") String usuario,
 			@JsonProperty(value="contrasena")String contrasena,
                         @JsonProperty(value="idOperador") Long idOperador,
                         @JsonProperty(value="nombre") String nombre) 
         {
-		super(usuario, contrasena, idOperador);
-		this.nombre = nombre;
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-        
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
+		super(usuario, contrasena, usuario, idOperador);
 	}
 }
